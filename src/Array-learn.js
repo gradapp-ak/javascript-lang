c1d130fb1ac45ab7c4300bbfa1e574b102a7b7d3
Karthick Ammasi <kammasi@netjets.com>
2018-07-10 13:59:48 -0400

    Added more learnings

--- src/Array-learn.js
+++ src/Array-learn.js
@@ -1,34 +1,57 @@
-var list = [1,2,3,4,13,341,89] //init
+var list = [1, 2, 3, 4, 13, 341, 89] //init
 
 list.push(675); //adds to the end
-console.log("List ="+list);
+console.log("List =" + list);
 
 var deleted = list.pop(675); //removes from the end
-console.log("List ="+list);
+console.log("List =" + list);
 
-list.shift();
-console.log("List="+list); //removed from the beginning
+var deletedOne = list.shift();
+console.log("List=" + list); //removed from the beginning
 
 var head = list.unshift(78);
-console.log("List="+list); //added to the beginning
+console.log("List=" + list); //added to the beginning
 
 //replace element in the middle
-var spliceOutput = list.splice(2,3,54);
-console.log("Splice List="+list); //remove elements starting from index 2, about 3 elements and replace it with 54added to the beginning
-console.log("Splice output="+spliceOutput);
+var spliceOutput = list.splice(2, 3, 54);
+console.log("Splice List=" + list); //remove elements starting from index 2, about 3 elements and replace it with 54added to the beginning
+console.log("Splice output=" + spliceOutput);
 
 //insert element in the middle
-var spliceOutput = list.splice(2,0,121);
-console.log("Splice List="+list); //remove elements starting from index 2, about 3 elements and replace it with 54added to the beginning
-console.log("Splice output="+spliceOutput);
+var spliceOutput = list.splice(2, 0, 121);
+console.log(`Splice List=${list}`); //remove elements starting from index 2, about 3 elements and replace it with 54added to the beginning
+console.log(`Splice output=${spliceOutput}`);
 
 //simply remove the element in the middle
-var spliceOutput = list.splice(2,1);
-console.log("Splice List="+list); //remove elements starting from index 2, about 3 elements and replace it with 54added to the beginning
-console.log("Splice output="+spliceOutput);
+var spliceOutput = list.splice(2, 1);
+console.log("Splice List=" + list); //remove elements starting from index 2, about 3 elements and replace it with 54added to the beginning
+console.log(`Splice output=${spliceOutput}`);
+
+//copy the array
+let l1 = [12, 23, 34, 41, 53, 60]
+
+let copied = l1.slice(1, 3)
+console.log("Copied=" + copied + ", Unchanged original=" + l1);
+
+//copy the entire array with spread operator
+let l2 = [...l1]
+console.log(`Spread Copied=${l2}`);
+
+//copy && combine array
+let l3 = [`a`, `b`, ...l1, 'we', ...l2, 'yui']
+console.log(`Combined array copy=${l3}`);
+
+let r1 = l3.find( e => e === 'a')
+console.log(`Find result=${r1}`)
+
+r1 = l3.find( e => e === 'huy')
+console.log(`Find result=${r1}`)
+
+r1 = l3.indexOf('we')
+console.log(`Index of result=${r1}`)
 
 //array loop
 for (var i = 0; i < list.length; i++) {
-   console.log(list[i]);
+    console.log(list[i]);
 }
 
--- src/Objects.js
+++ src/Objects.js
@@ -1,15 +1,39 @@
 var anObject = {
-  make: "Ford",
-  5: "five",
-  "model": "focus"
+    make: "Ford",
+    5: "five",
+    "model": "focus"
 }; //string, number && for simplicity a string with no quotes are all valid property of the object
 
-console.log("Object="+anObject[5]); //["5"] also works, but not anObject.5
-console.log("Object="+anObject["make"]);
-console.log("Object="+anObject.model);
+console.log("Object=" + anObject[5]); //["5"] also works, but not anObject.5
+console.log("Object=" + anObject["make"]);
+console.log("Object=" + anObject.model);
 
 //delete a property
 delete anObject.make;
-console.log("Object="+anObject["make"]);
+console.log("Object=" + anObject["make"]);
 
-console.log("Has own property, 5? ="+anObject.hasOwnProperty("5"));
+console.log("Has own property, 5? =" + anObject.hasOwnProperty("5"));
+
+var theObject = {
+    a: "a",
+    b: `p`,
+    c: `we`
+}
+
+//iterate over the object
+for (var e in theObject) {
+    console.log(`element=${e}`)
+}
+
+let keys = Object.keys(theObject)
+console.log(keys);
+
+//iterate over all the keys of an Object using OF
+for (var key of keys) {
+    console.log(`key=${key}`)
+}
+
+//iterate over all the keys of an Object using IN
+for(let key in keys){
+    console.log(`Keys using 'in'=${key}`)
+}
